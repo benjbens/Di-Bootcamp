ceec5753a367b1bce0e7243a33da27cbc83e02ad
Benjamin Bensamoun <benstreet@MBP-de-Benjamin.lan>
2022-03-20 15:29:36 +0200

    day1 week2

---
+++ week2/day1(2)/exercice/exercice.js
@@ -0,0 +1,173 @@
+
+// Exercice 1
+let favoritefood = "chocolate";
+let favoritemeal ="dinner";
+console.log(favoritefood)
+console.log(favoritemeal)
+
+let globalmeal = `I eat ${favoritefood} at evry ${favoritemeal} `;
+console.log(globalmeal)
+
+
+
+
+// Exercice 2
+let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+let myWatchedSeriesLength = myWatchedSeries.length;
+console.log (`I watched 3 series: ${myWatchedSeries} `);
+myWatchedSeries.splice(2,1);
+console.log(myWatchedSeries)
+myWatchedSeries.push("Friends")
+console.log(myWatchedSeries)
+myWatchedSeries.splice(0,1, "Game of throne")
+console.log(myWatchedSeries)
+console.log(myWatchedSeries[1][2]);
+
+
+// Exercice 3
+
+let c = 22;
+let f = ((c / 5) * 9) + 32;
+console.log(`${c}°C = ${f}°F`);
+
+
+// Exercice 4
+let a = 34;
+let b = 21;
+console.log(a+b)
+console.log(3 + 4 + '5');
+
+// 1=55
+// 2=23
+// 3=undifind
+// 4=75
+
+// Exercice 5 
+
+typeof(15)
+// Prediction: number
+// Actual: number
+
+typeof(5.5)
+// Prediction: number
+// Actual: number
+
+typeof(NaN)
+// Prediction: string
+// Actual: number
+
+typeof("hello")
+// Prediction: string
+// Actual: string
+
+typeof(true)
+// Prediction: boolean
+// Actual: boolean
+
+typeof(1 != 2)
+// Prediction: boolean
+// Actual: boolean
+
+"hamburger" + "s"
+// Prediction: hamburgers
+// Actual: hamburgers
+
+"hamburgers" - "s"
+// Prediction: nan
+// Actual: nan
+
+"1" + "3"
+// Prediction: 4
+// Actual: 13
+
+"1" - "3"
+// Prediction: -2
+// Actual: -2
+
+"johnny" + 5
+// Prediction:
+// Actual:
+
+"johnny" - 5
+// Prediction: johnny5
+// Actual: johnny5
+
+99 * "hello"
+// Prediction: nan
+// Actual: nan
+
+1 != 1
+// Prediction: false
+// Actual: false
+
+1 == "1"
+// Prediction: true
+// Actual: true
+
+1 === "1"
+// Prediction: false
+// Actual: flase
+
+
+
+// Exercice 6
+
+
+5 + "34"
+// Prediction: 534
+// Actual: 534
+
+5 - "4"
+// Prediction: 1
+// Actual: 1
+
+10 % 5
+// Prediction: ?
+// Actual: 0
+
+5 % 10
+// Prediction: 5
+// Actual: 5
+
+"Java" + "Script"
+// Prediction: JavaScript
+// Actual: JavaScript
+
+" " + " "
+// Prediction: ?
+// Actual: , ,
+
+" " + 0
+// Prediction: ' 0'
+// Actual: ' 0'
+
+true + true
+// Prediction: 2
+// Actual: 2
+
+true + false
+// Prediction: 1
+// Actual: 1
+
+false + true
+// Prediction: 1
+// Actual: 1
+
+false - true
+// Prediction: -1
+// Actual: -1
+
+!true
+// Prediction: false
+// Actual: flase
+
+3 - 4
+// Prediction: -1
+// Actual: -1
+
+"Bob" - "bill"
+// Prediction: ?
+// Actual: NaN
+	
+
+
