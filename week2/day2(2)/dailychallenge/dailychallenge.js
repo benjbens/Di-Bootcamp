ab8d8c054b64163af9065e50851d48b736385855
Benjamin Bensamoun <benstreet@MBP-de-Benjamin.lan>
2022-03-21 15:38:17 +0200

    dailychallenge day2

---
+++ week2/day2(2)/dailychallenge/dailychallenge.js
@@ -0,0 +1,8 @@
+
+let sen = `The moovie is not that bas, I like it`;
+let arr = sen.split(` `)
+
+let not = arr.indexOf(`not`);
+let bad = arr.indexOf(`bad`);
+arr.splice(not,bad-not+3,`good`);
+console.log(arr.join(` `));
