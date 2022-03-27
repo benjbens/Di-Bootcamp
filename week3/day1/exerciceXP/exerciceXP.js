9b399bdd927132079948dc9d4a03dec6743343d0
Benjamin Bensamoun <benstreet@MBP-de-Benjamin.lan>
2022-03-27 15:57:15 +0300

    week3

---
+++ week3/day1/exerciceXP/exerciceXP.js
@@ -0,0 +1,93 @@
+
+// Exercice 1
+
+
+// let socialNetworkNavigation = document.getElementById("navBar");
+// socialNetworkNavigation.setAttribute("id","socialNetworkNavigation");
+// console.log(socialNetworkNavigation)
+
+
+
+
+// let ulParents = document.getElementsByTagName("ul")
+// let liChild = document.createElement("li");
+// let textLi = document.createTextNode("Logout");
+// liChild.appendChild(textLi);
+// ulParents[0].appendChild(liChild);
+
+
+
+// Exercice 2
+
+
+// let richard = document.getElementsByTagName("ul");
+// console.log(richard)
+// let getrichard = richard[0];
+// let robert = getrichard.lastElementChild
+// console.log(robert);
+// robert.textContent ="Richard"
+
+// let Benji = document.getElementsByTagName("ul");
+// let Benji1 = document.getElementsByTagName("ul");
+// console.log(Benji);
+// console.log(Benji1);
+// let getBenji = Benji[0];
+// let getBenji1 = Benji1[1];
+// getBenji.firstElementChild.textContent ="Benji"
+// getBenji1.firstElementChild.textContent ="Benji"
+
+// let ulParents = document.getElementsByTagName("ul")
+// let liChild = document.createElement("li");
+// let textLi = document.createTextNode("Hey students");
+// liChild.appendChild(textLi);
+// ulParents[0].appendChild(liChild);
+
+
+
+// let UlParents = document.getElementsByTagName("ul")
+// let LiChild = document.createElement("li");
+// let TextLi = document.createTextNode("Hey students");
+// LiChild.appendChild(TextLi);
+// UlParents[1].appendChild(LiChild);
+
+// console.log(UlParents[1].children)
+
+// let Parents = document.getElementsByTagName("ul");
+// let Salut = Parents[1];
+// let getULparents = Salut.children[1].remove()
+
+
+// Exercice 3
+
+
+
+document.body.firstElementChild.style.backgroundColor = "lightblue";
+document.body.firstElementChild.style.padding = "50px";
+
+let Parents = document.getElementsByTagName("ul");
+let John = Parents[0];
+let getParents = John.children[0].remove()
+
+
+let parents = document.getElementsByTagName("ul");
+let pete = parents[0];
+console.log(pete.firstElementChild)
+pete.firstElementChild.style.border = "5px solid #000";
+
+document.body.style.fontSize = "medium";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
