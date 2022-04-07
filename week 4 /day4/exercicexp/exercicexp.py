4989dade66cfaf1a68c550651ffa94c27a8df06e
Benjamin Bensamoun <benstreet@MBP-de-Benjamin.lan>
2022-04-07 11:14:03 +0300

    tic tac toe

--- week 4 /day4/exercicexp/exercicexp.py
+++ week 4 /day4/exercicexp/exercicexp.py
@@ -1,13 +1,13 @@
-from random import *
+
 
 # Exercice 1
 
 
 
-def display_message():
-    print("python")
+# def display_message():
+#     print("python")
 
-display_message()    
+# display_message()    
 
 
 # Exercice 2 
@@ -15,22 +15,22 @@ display_message()
 
 
 
-def favorite_book(title):
-    print(f"One of my favorite books is {title}")
+# def favorite_book(title):
+#     print(f"One of my favorite books is {title}")
 
 
-favorite_book("1984")
+# favorite_book("1984")
 
 
 # Exercice 3 
 
-city = "paris"
-country = "france"
+# city = "paris"
+# country = "france"
 
-def describe_city():
-    print(f"{city} is in {country}")
+# def describe_city():
+#     print(f"{city} is in {country}")
 
-describe_city()
+# describe_city()
 
 # Exercice 4 
 
@@ -65,24 +65,83 @@ describe_city()
 
 # Exercice 6 
 
-magiciens = ["fernando", "Jojo", "Jackie"]
+# magiciens = ["fernando", "Jojo", "Jackie"]
 
-def show_magicians(magiciens):
+# def show_magicians(magiciens):
     
-    for i in magiciens:
-        print(i)
+#     for i in magiciens:
+#         print(i)
 
-show_magicians(magiciens)
-
-
-
-def make_great(magiciens):
-
- new_list = ["the great " + i for i in magiciens]
- print(new_list)
+# show_magicians(magiciens)
 
 
-make_great(magiciens)
+
+# def make_great(magiciens):
+
+#  new_list = ["the great " + i for i in magiciens]
+#  print(new_list)
+
+
+# make_great(magiciens)
+
+
+matrix = [['7', 'i', '3'],
+          ['T','s','i'],
+          ['h','%','x'],
+          ['i',' ','#'],
+          ['s','M',' '],
+          ['$','a',' '],
+          ['#','t','%'],
+          ['^','r','!']]
+result = ""
+#len(matrix[0]) -> 3
+special_chars = 0
+for column in range(len(matrix[0])):
+    #len(matrix) -> 8
+    for row in range(len(matrix)):
+        print(matrix[row][column])
+
+        if matrix[row][column].isalpha():
+            result += matrix[row][column] 
+            special_chars = 0
+
+        else:
+            if special_chars == 2 or matrix[row][column] == " ":
+                result += " "
+            else: 
+                special_chars += 1
+
+
+
+
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
