e6c5b447b85ff80966375f7e8f9c3aeaf2082c6c
benjbens <101180079+benjbens@users.noreply.github.com>
2022-04-12 16:33:19 +0300

    exe

---
+++ week5/day3/exercicexp/exercicexp2.py
@@ -0,0 +1,42 @@
+# import datetime
+
+
+
+
+
+# EXercice 1 
+
+# def current_date():
+#     actual_datetime = datetime.datetime.now()
+#     print(actual_datetime)
+
+# current_date()
+
+
+# Exercice 2
+
+# def amount_of():
+#     actual_datetime = datetime.datetime.now()
+#     next_year = actual_datetime.year + 1
+#     date_time = datetime.datetime(next_year, 1, 1)
+#     time_left =  date_time - actual_datetime 
+#     print(f'{time_left} day left')
+
+
+# amount_of()
+
+
+# Exercice 3
+
+# def amount_of(year, month, day):
+#     actual_datetime = datetime.datetime.now()
+#     date_born = datetime.datetime(year, month, day)
+#     years_alive = actual_datetime - date_born
+#     minutes = int(years_alive.total_seconds()/ 60) 
+#     print(minutes)
+
+
+# amount_of(2003, 3, 4)
+
+
+# Exercice 6
